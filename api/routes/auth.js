c2ee9eca0508443f22c70c9f4280f5793eb21c43
DSankaram <sankaram@octaloop.com>
2023-04-01 13:54:38 +0530

    Auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,10 @@
 import express from "express";
 import {addPost} from "../controllers/post.js";
+import {login, logout, register} from "../controllers/auth.js";
 
 const router = express.Router()
 
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
 export default router
